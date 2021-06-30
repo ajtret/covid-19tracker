905718666dc67f45d20fda7d9bb9c121871a2794
ajtret <86714582+ajtret@users.noreply.github.com>
2021-06-30 15:01:53 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6qGbW-jh6HaeZFeEUayq88qsRnyfNOtw",
+    authDomain: "covid19-tracker-595ea.firebaseapp.com",
+    projectId: "covid19-tracker-595ea",
+    storageBucket: "covid19-tracker-595ea.appspot.com",
+    messagingSenderId: "140319288951",
+    appId: "1:140319288951:web:3d49ddc4f9484de35ca072"
+  };
